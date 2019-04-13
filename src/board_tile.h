f58de59fbe7a8b8a44befe39a009c91259c3ec2d
eliaskauppi <elkku.kauppi@gmail.com>
2019-04-13 20:43:08 +0300

    noexcepts, better constructor and comments.

--- src/board_tile.h
+++ src/board_tile.h
@@ -5,6 +5,9 @@ namespace msgn {
  * Contains data of single tile placed on a board. Such information is: is the
  * tile open, is flag set on it and it's holding value which is either empty
  * represented by 0, number between 1-8 or mine represented by 9.
+ * 
+ * Consumes 2 bytes compiled with g++. This could be size optimized to 1 byte, but
+ * there could be greater loss when decoding wanted results.
  */
 class BoardTile {
 public:
@@ -19,21 +22,24 @@ public:
                        TILE_8 = 8, TILE_MINE = 9;
 
   BoardTile() : tile_value(TILE_EMPTY), b_flagged(false), b_open(false) {}
-  BoardTile(tile_value_type tile_value_) : tile_value(tile_value_) {}
+  BoardTile(tile_value_type tile_value_) : tile_value(tile_value_) {
+    set_closed();
+    set_unflagged();
+  }
   BoardTile(tile_value_type tile_value_, bool is_flagged, bool is_open)
       : tile_value(tile_value_), b_flagged(is_flagged), b_open(is_open) {}
   ~BoardTile() noexcept {}
 
-  bool is_mine() const { return tile_value == TILE_MINE; }
-  bool is_empty() const { return tile_value == TILE_EMPTY; }
-  void set_mine() { tile_value = TILE_MINE; }
-  void set_empty() { tile_value = TILE_EMPTY; }
-  void set_open() { b_open = true; }
-  void set_closed() { b_open = false; }
-  void set_flagged() { b_flagged = true; }
-  void set_unflagged() { b_flagged = false; }
+  bool is_mine() const noexcept { return tile_value == TILE_MINE; }
+  bool is_empty() const noexcept { return tile_value == TILE_EMPTY; }
+  void set_mine() noexcept { tile_value = TILE_MINE; }
+  void set_empty() noexcept { tile_value = TILE_EMPTY; }
+  void set_open() noexcept { b_open = true; }
+  void set_closed() noexcept { b_open = false; }
+  void set_flagged() noexcept { b_flagged = true; }
+  void set_unflagged() noexcept { b_flagged = false; }
 
-  void reset() {
+  void reset() noexcept {
     set_closed();
     set_unflagged();
     set_empty();
