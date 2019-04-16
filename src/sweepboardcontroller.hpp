a39f41d03cf9156a65d7fb432915bda371434264
eliaskauppi <elkku.kauppi@gmail.com>
2019-04-16 21:35:03 +0300

    added default constructor and renamed variable

--- src/sweepboardcontroller.hpp
+++ src/sweepboardcontroller.hpp
@@ -1,17 +1,18 @@
-#include "sweepboard.h"
-#include "board_tile.h"
+#include "board_tile.hpp"
+#include "sweepboard.hpp"
 
 namespace msgn {
 
 class SweepBoardController {
 private:
-  SweepBoard board;
+  SweepBoard m_board;
 
 public:
+  SweepBoardController() : m_board(0,0,0) {}
   SweepBoardController(std::size_t width_, std::size_t height_,
                        double mine_fill_percent,
                        uint32_t seed = std::mt19937_64::default_seed)
-      : board(width_, height_, mine_fill_percent, seed) {}
+      : m_board(width_, height_, mine_fill_percent, seed) {}
   ~SweepBoardController() {}
 
   void foo() {}
