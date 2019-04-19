4ad53028f548cbfe3d18da6aa529a7eb1e9f2028
eliaskauppi <elkku.kauppi@gmail.com>
2019-04-19 12:02:01 +0300

    testing window functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,19 @@
-#include <algorithm>
 #include <chrono>
 #include <iostream>
 #include <random>
-#include <utility>
-#include <vector>
 
-#include "sweepboard.h"
+#include "SDL2/SDL.h"
 
-int main() {
-  msgn::SweepBoard sb(71, 49, .05, time(0));
+#include "gamemanager.hpp"
+#include "sweepboard.hpp"
+#include "sweepboardformat.hpp"
+#include "sweepboardcontroller.hpp"
+#include "windowmanager.hpp"
 
-  auto t1 = std::chrono::high_resolution_clock::now();
-  sb.m_open_neighbours(sb.m_empty_tiles_empty_area(439));
-  auto t2 = std::chrono::high_resolution_clock::now();
+int main(int argc, char *argv[]) {
+  SDL_Init(SDL_INIT_VIDEO);
+  msgn::WindowManager wm;
+  wm.create_window("test", 480,320,100,100,0);
 
-  std::cout
-      << "Old: "
-      << std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count()
-      << " ns\n";
-
-  t1 = std::chrono::high_resolution_clock::now();
-  sb.m_open_neighbours(sb.m_empty_tiles_empty_area_test(439));
-  t2 = std::chrono::high_resolution_clock::now();
-
-  std::cout
-      << "New: "
-      << std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count()
-      << " ns\n";
-
-  t1 = std::chrono::high_resolution_clock::now();
-  sb.m_flood_queue(439);
-  t2 = std::chrono::high_resolution_clock::now();
-
-  std::cout
-      << "flood_queue: "
-      << std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count()
-      << " ns\n";
-
-  t1 = std::chrono::high_resolution_clock::now();
-  sb.m_flood_stack(439);
-  t2 = std::chrono::high_resolution_clock::now();
-
-  std::cout
-      << "flood_stack: "
-      << std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count()
-      << " ns\n";
   return 0;
 }
