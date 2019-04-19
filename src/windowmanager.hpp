75aeadaacc62525471aea59bff3edb7bf6275516
eliaskauppi <elkku.kauppi@gmail.com>
2019-04-19 12:04:47 +0300

    extended WindowManager class

--- src/windowmanager.hpp
+++ src/windowmanager.hpp
@@ -1,8 +1,8 @@
 #ifndef WINDOWMANAGER_HPP
 #define WINDOWMANAGER_HPP
 
-#include <string_view>
 #include <string>
+#include <string_view>
 
 #include "SDL2/SDL.h"
 
@@ -14,27 +14,45 @@ public:
   using diff_type = long;
 
 private:
-  SDL_Window *m_win;
-  SDL_Renderer *m_rndrer;
+  SDL_Window *m_window;
+  SDL_Renderer *m_renderer;
+  SDL_Surface *m_surface;
 
   size_type m_width, m_height;
+  volatile bool done;
 
 public:
-  WindowManager() { SDL_Init(SDL_INIT_VIDEO); }
+  WindowManager()
+      : m_window(nullptr), m_renderer(nullptr), m_width(0), m_height(0) {}
   ~WindowManager() {
-    if (m_rndrer != nullptr)
-      SDL_DestroyRenderer(m_rndrer);
-    if (m_win != nullptr)
-      SDL_DestroyWindow(m_win);
+    if (m_renderer != nullptr)
+      SDL_DestroyRenderer(m_renderer);
+    if (m_window != nullptr)
+      SDL_DestroyWindow(m_window);
   }
 
   void create_window(std::string_view title, diff_type width, diff_type height,
                      diff_type pos_x, diff_type pos_y, uint32_t flags) {
-    m_win = SDL_CreateWindow(title.data(), pos_x, pos_y, width, height, flags);
+    m_window =
+        SDL_CreateWindow(title.data(), pos_x, pos_y, width, height, flags);
+    if (m_window == nullptr)
+      SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Window creation fail : %s\n",
+                   SDL_GetError());
+    m_surface = SDL_GetWindowSurface(m_window);
+    m_renderer = SDL_CreateSoftwareRenderer(m_surface);
+    if (m_renderer == nullptr)
+      SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
+                   "Render creation for surface fail : %s\n", SDL_GetError());
+    SDL_SetRenderDrawColor(m_renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+    SDL_RenderClear(m_renderer);
+  }
+
+  operator SDL_Renderer*() {
+    return m_renderer;
   }
 
 private:
-};
+}; // class WindowManager
 
 } // namespace msgn
 
