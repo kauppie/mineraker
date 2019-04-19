56a5b83c323e7727ece3737c158b50645774c7b7
eliaskauppi <elkku.kauppi@gmail.com>
2019-04-19 11:47:43 +0300

    refactored

--- src/boardtile.hpp
+++ src/boardtile.hpp
@@ -14,39 +14,44 @@ namespace msgn {
 class BoardTile {
 public:
   using constant_type = const unsigned char;
-  using tile_value_type = unsigned char;
+  using value_type = unsigned char;
 
-  // Defines if board tile is opened.
-  bool b_open : 1;
+  // Defines tile's board value. Ranges from 0 to 9. 9 represents a mine.
+  value_type tile_value;
   // Defines if board tile is flagged.
   bool b_flagged : 1;
-  // Defines tile's board value. Ranges from 0 to 9. 9 represents a mine.
-  tile_value_type tile_value;
+  // Defines if board tile is opened.
+  bool b_open : 1;
 
   static constant_type TILE_EMPTY = 0, TILE_1 = 1, TILE_2 = 2, TILE_3 = 3,
                        TILE_4 = 4, TILE_5 = 5, TILE_6 = 6, TILE_7 = 7,
                        TILE_8 = 8, TILE_MINE = 9;
 
   // @brief Default constructor that sets tile to cleared one.
-  constexpr BoardTile() : tile_value(TILE_EMPTY), b_flagged(false), b_open(false) {}
+  constexpr BoardTile()
+      : tile_value(TILE_EMPTY), b_flagged(false), b_open(false) {}
   // @brief Constructor that takes tile value as parameter. Tile is set to
   // resetted one.
-  constexpr BoardTile(tile_value_type tile_value)
-      : tile_value(tile_value), b_open(false), b_flagged(false) {}
+  constexpr BoardTile(value_type tile_value)
+      : tile_value(tile_value), b_flagged(false), b_open(false) {}
   // @brief Constructor that takes all tile defining values as parameters and
   // constructs variables from them.
-  constexpr BoardTile(tile_value_type tile_value, bool is_flagged, bool is_open)
+  constexpr BoardTile(value_type tile_value, bool is_flagged, bool is_open)
       : tile_value(tile_value), b_flagged(is_flagged), b_open(is_open) {}
   // @brief Deconstructor.
   ~BoardTile() noexcept {}
 
-  // @brief Returns true when tile is mine. False otherwise.
+  // @brief Returns true when tile is a mine. False otherwise.
   constexpr bool is_mine() const noexcept { return tile_value == TILE_MINE; }
   // @brief Returns true when tile is empty. False otherwise.
   constexpr bool is_empty() const noexcept { return tile_value == TILE_EMPTY; }
+  // @brief Returns true when tile is neither empty or a mine. False otherwise.
+  constexpr bool is_number() const noexcept {
+    return !is_mine() && !is_empty();
+  }
   // @brief Sets tile to a mine.
   constexpr void set_mine() noexcept { tile_value = TILE_MINE; }
-  // @brief Sets tile to a empty tile.
+  // @brief Sets tile to empty tile.
   constexpr void set_empty() noexcept { tile_value = TILE_EMPTY; }
   // @brief Opens tile.
   constexpr void set_open() noexcept { b_open = true; }
@@ -59,8 +64,7 @@ public:
 
   // @brief Clears BoardTile to have value of closed, unflagged and empty tile.
   constexpr void clear() noexcept {
-    set_closed();
-    set_unflagged();
+    reset();
     set_empty();
   }
 
