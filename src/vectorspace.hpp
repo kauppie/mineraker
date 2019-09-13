d9f966891c411fe6374e6a5e344a6ab265090062
eliaskauppi <elkku.kauppi@gmail.com>
2019-09-14 01:20:50 +0300

    VectorSpace modified to reflect more std::vector methods.

--- src/vectorspace.hpp
+++ src/vectorspace.hpp
@@ -1,6 +1,7 @@
 #ifndef VECTORSPACE_HPP
 #define VECTORSPACE_HPP
 
+#include <exception>
 #include <memory>
 #include <vector>
 
@@ -16,24 +17,45 @@ template <typename T> class VectorSpace;
  */
 template <typename T> class VecSpaceInstance {
   using vec_type = std::vector<T>;
-  using reference = vec_type &;
-  using const_reference = const vec_type &;
   using pointer_type = std::shared_ptr<vec_type>;
 
 public:
   explicit VecSpaceInstance(const pointer_type &vec) { m_vector = vec; }
-  explicit VectorSpaceInstance() = delete;
-  ~VecSpaceInstance() {}
+  ~VecSpaceInstance() { m_vector->resize(0); }
 
+  // @brief Index operator to access element at given index.
+  const T &operator[](size_type idx) const { return m_vector->operator[](idx); }
+
+  // @brief Index operator to access element at given index.
   T &operator[](size_type idx) { return m_vector->operator[](idx); }
 
+  // @brief Index operator to access element at given index. %idx is bounds
+  // checked.
+  const T &at(size_type idx) const {
+    if (idx >= m_vector->size())
+      throw std::out_of_range("Index is out of range.");
+    return m_vector->operator[](idx);
+  }
+
+  // @brief Index operator to access element at given index. %idx is bounds
+  // checked.
+  T &at(size_type idx) {
+    if (idx >= m_vector->size())
+      throw std::out_of_range("Index is out of range.");
+    return m_vector->operator[](idx);
+  }
+
   // @brief Get function. It's undefined to call returned vector's destructor.
-  reference get() { return *m_vector.get(); }
+  vec_type &get() { return *m_vector.get(); }
 
   // @brief Constant get function.
-  const_reference get() const { return *m_vector.get(); }
+  const vec_type &get() const { return *m_vector.get(); }
 
-  void emplace(const T &other) { m_vector->emplace_back(other); }
+  auto front() const { return m_vector->front(); }
+  auto begin() const { return m_vector->begin(); }
+  auto end() const { return m_vector->end(); }
+
+  void emplace_back(const T &other) { m_vector->emplace_back(other); }
 
   auto size() const { return m_vector->size(); }
 
@@ -64,18 +86,20 @@ public:
     m_reserve_size = reserve;
   }
 
-  auto vectors_reserve() const { return m_vec_list.front()->capacity(); }
+  auto vectors_reserve() const { return m_reserve_size; }
 
   void space_size(size_type vec_num) {
     m_vec_list.resize(vec_num);
-    vectors_reserve(m_reserve_size);
+    for (auto &vec_p : m_vec_list)
+      vec_p = std::make_shared<vec_type>();
+    if (m_reserve_size > 0)
+      vectors_reserve(m_reserve_size);
   }
 
   auto space_size() const { return m_vec_list.size(); }
 
   instance_type acquire() {
     for (auto &vec_p : m_vec_list) {
-      // std::shared_ptr.unique() is going to deprecate from C++20.
       if (vec_p.use_count() == 1)
         return instance_type(vec_p);
     }
