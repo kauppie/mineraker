3423ebbe6946b771921af3a09c7458d19ed4f459
eliaskauppi <elkku.kauppi@gmail.com>
2020-03-07 20:23:40 +0200

    Solver algorithms' major bugs fixed

--- src/solver_iterator.hpp
+++ src/solver_iterator.hpp
@@ -16,149 +16,178 @@ public:
 
   using pos_type = Position_t;
 
-  SolverIterator() {}
+  SolverIterator(std::shared_ptr<Mineboardbase> ptr) : board_ptr(ptr) {}
   ~SolverIterator() {}
 
-  bool next_step() { return next_flag_step() || next_open_step(); }
-
-  bool next_flag_step() {
-    bool next_change = false;
-    if (auto b_ptr = board_ptr.lock()) {
-
-      for (auto tile_it = b_ptr->begin(); tile_it != b_ptr->end(); ++tile_it) {
-        if (tile_it->is_open() && tile_it->is_number()) {
-          auto neighbours = b_ptr->tile_neighbours(
-              std::distance(b_ptr->begin(), tile_it),
-              [&b_ptr](const pos_type& p) {
-                return b_ptr->is_inside_bounds(p) && !b_ptr->at(p).is_flagged();
-              });
-
-          if (tile_it->value() ==
-              std::count_if(neighbours.begin(), neighbours.end(),
-                            [&b_ptr](const pos_type& p) {
-                              return !b_ptr->at(p).is_open();
-                            })) {
-            std::for_each(
-                neighbours.begin(), neighbours.end(),
-                [&b_ptr](const pos_type& p) { b_ptr->at(p).set_flagged(); });
-            next_change = true;
-          } else {
-            for (auto neigh_pos_it = neighbours.begin();
-                 neigh_pos_it != neighbours.end(); ++neigh_pos_it) {
-              auto tile_flag_value =
-                  tile_it->value() -
-                  std::count_if(neighbours.begin(), neighbours.end(),
-                                [&b_ptr](const pos_type& p) {
-                                  return b_ptr->at(p).is_flagged();
-                                });
-              auto neigh_neighbours = b_ptr->tile_neighbours(*neigh_pos_it);
-              auto neighbour_flag_value =
-                  b_ptr->at(*neigh_pos_it).value() -
-                  std::count_if(neigh_neighbours.begin(),
-                                neigh_neighbours.end(),
-                                [&b_ptr](const pos_type& p) {
-                                  return b_ptr->at(p).is_flagged();
-                                });
-
-              if (b_ptr->at(*neigh_pos_it).is_open() &&
-                  b_ptr->at(*neigh_pos_it).is_number() &&
-                  tile_flag_value > neighbour_flag_value) {
-                std::vector<pos_type> diff;
-                std::set_difference(neighbours.begin(), neighbours.end(),
-                                    neigh_neighbours.begin(),
-                                    neigh_neighbours.end(),
-                                    std::back_inserter(diff));
-                auto diff_mines = tile_flag_value - neighbour_flag_value;
-
-                if (diff.size() == diff_mines) {
-                  std::copy(diff.begin(), diff.end(),
-                            std::back_inserter(next_flag));
-                  next_change = true;
-                }
-              }
-            }
-          }
-        }
-      }
-
-    } else {
-      throw std::bad_weak_ptr();
-    }
-    return next_change;
+  void next_step() {
+    next_flag_step();
+    next_open_step();
   }
 
-  bool next_open_step() {
-    bool next_change = false;
-    if (auto b_ptr = board_ptr.lock()) {
-      for (auto tile_it = b_ptr->begin(); tile_it != b_ptr->end(); ++tile_it) {
-        if (tile_it->is_open() && tile_it->is_number()) {
-          auto neighbours = b_ptr->tile_neighbours(
-              std::distance(b_ptr->begin(), tile_it),
-              [&b_ptr](const pos_type& p) {
-                return b_ptr->is_inside_bounds(p) && !b_ptr->at(p).is_flagged();
-              });
+  void next_flag_step() {
+    for (auto tile_it = board_ptr->begin(); tile_it != board_ptr->end();
+         ++tile_it) {
+      if (tile_it->is_open() && tile_it->is_number()) {
 
-          if (tile_it->value() ==
-              std::count_if(neighbours.begin(), neighbours.end(),
-                            [&b_ptr](const pos_type& p) {
-                              return b_ptr->at(p).is_flagged();
-                            })) {
-            std::for_each(
-                neighbours.begin(), neighbours.end(),
-                [&b_ptr](const pos_type& p) { b_ptr->at(p).set_open(); });
-            next_change = true;
-          } else {
-            for (auto neigh_pos_it = neighbours.begin();
-                 neigh_pos_it != neighbours.end(); ++neigh_pos_it) {
-              auto tile_flag_value =
-                  tile_it->value() -
-                  std::count_if(neighbours.begin(), neighbours.end(),
-                                [&b_ptr](const pos_type& p) {
-                                  return b_ptr->at(p).is_flagged();
-                                });
+        auto all_neighbours = board_ptr->tile_neighbours(
+            std::distance(board_ptr->begin(), tile_it));
 
-              auto neigh_neighbours = b_ptr->tile_neighbours(*neigh_pos_it);
-              auto neighbour_flag_value =
-                  b_ptr->at(*neigh_pos_it).value() -
-                  std::count_if(neigh_neighbours.begin(),
-                                neigh_neighbours.end(),
-                                [&b_ptr](const pos_type& p) {
-                                  return b_ptr->at(p).is_flagged();
-                                });
-              if (b_ptr->at(*neigh_pos_it).is_open() &&
-                  b_ptr->at(*neigh_pos_it).is_number() &&
-                  tile_flag_value == neighbour_flag_value &&
-                  neighbours.size() > neigh_neighbours.size() &&
-                  std::includes(neighbours.begin(), neighbours.end(),
-                                neigh_neighbours.begin(),
-                                neigh_neighbours.end())) {
-                std::set_difference(neighbours.begin(), neighbours.end(),
-                                    neigh_neighbours.begin(),
-                                    neigh_neighbours.end(),
-                                    std::back_inserter(next_open));
-                next_change = true;
+        if (tile_it->value() ==
+            std::count_if(all_neighbours.begin(), all_neighbours.end(),
+                          [this](const pos_type& p) {
+                            return !board_ptr->at(p).is_open();
+                          })) {
+          std::copy(all_neighbours.begin(), all_neighbours.end(),
+                    std::back_inserter(next_flag));
+        } else {
+          for (auto neigh_pos_it = all_neighbours.begin();
+               neigh_pos_it != all_neighbours.end(); ++neigh_pos_it) {
+            auto tile_flag_value =
+                tile_it->value() -
+                std::count_if(all_neighbours.begin(), all_neighbours.end(),
+                              [this](const pos_type& p) {
+                                return board_ptr->at(p).is_flagged();
+                              });
+            auto all_neigh_neighbours =
+                board_ptr->tile_neighbours(*neigh_pos_it);
+            auto neighbour_flag_value =
+                board_ptr->at(*neigh_pos_it).value() -
+                std::count_if(all_neigh_neighbours.begin(),
+                              all_neigh_neighbours.end(),
+                              [this](const pos_type& p) {
+                                return board_ptr->at(p).is_flagged();
+                              });
+
+            if (board_ptr->at(*neigh_pos_it).is_open() &&
+                board_ptr->at(*neigh_pos_it).is_number() &&
+                tile_flag_value > neighbour_flag_value) {
+              std::vector<pos_type> diff;
+              auto not_fl_op_neighbours = not_flagged_opened_neighbours(
+                  std::distance(board_ptr->begin(), tile_it));
+              auto neigh_not_fl_op_neighbours =
+                  not_flagged_opened_neighbours(*neigh_pos_it);
+              std::set_difference(
+                  not_fl_op_neighbours.begin(), not_fl_op_neighbours.end(),
+                  neigh_not_fl_op_neighbours.begin(),
+                  neigh_not_fl_op_neighbours.end(), std::back_inserter(diff));
+              auto diff_mines = tile_flag_value - neighbour_flag_value;
+
+              if (diff.size() == diff_mines) {
+                std::copy(diff.begin(), diff.end(),
+                          std::back_inserter(next_flag));
               }
             }
           }
         }
       }
-    } else {
-      throw std::bad_weak_ptr();
     }
-    return next_change;
+  }
+
+  void next_open_step() {
+
+    for (auto tile_it = board_ptr->begin(); tile_it != board_ptr->end();
+         ++tile_it) {
+      if (tile_it->is_open() && tile_it->is_number()) {
+
+        auto not_fl_op_neighbours = not_flagged_opened_neighbours(
+            std::distance(board_ptr->begin(), tile_it));
+        auto all_neighbours = board_ptr->tile_neighbours(
+            std::distance(board_ptr->begin(), tile_it));
+
+        auto tile_flag_value =
+            tile_it->value() -
+            std::count_if(all_neighbours.begin(), all_neighbours.end(),
+                          [this](const pos_type& p) {
+                            return board_ptr->at(p).is_flagged();
+                          });
+
+        if (tile_flag_value ==
+            std::count_if(not_fl_op_neighbours.begin(),
+                          not_fl_op_neighbours.end(),
+                          [this](const pos_type& p) {
+                            return board_ptr->at(p).is_flagged();
+                          })) {
+          std::copy(not_fl_op_neighbours.begin(), not_fl_op_neighbours.end(),
+                    std::back_inserter(next_open));
+        } else {
+          for (auto neigh_pos_it = all_neighbours.begin();
+               neigh_pos_it != all_neighbours.end(); ++neigh_pos_it) {
+
+            if (board_ptr->at(*neigh_pos_it).is_open() &&
+                board_ptr->at(*neigh_pos_it).is_number()) {
+
+              auto neigh_not_fl_op_neighbours =
+                  not_flagged_opened_neighbours(*neigh_pos_it);
+              auto all_neigh_neighbours =
+                  board_ptr->tile_neighbours(*neigh_pos_it);
+              auto neighbour_flag_value =
+                  board_ptr->at(*neigh_pos_it).value() -
+                  std::count_if(all_neigh_neighbours.begin(),
+                                all_neigh_neighbours.end(),
+                                [this](const pos_type& p) {
+                                  return board_ptr->at(p).is_flagged();
+                                });
+              if (tile_flag_value == neighbour_flag_value &&
+                  not_fl_op_neighbours.size() >
+                      neigh_not_fl_op_neighbours.size() &&
+                  std::includes(not_fl_op_neighbours.begin(),
+                                not_fl_op_neighbours.end(),
+                                neigh_not_fl_op_neighbours.begin(),
+                                neigh_not_fl_op_neighbours.end())) {
+
+                std::set_difference(not_fl_op_neighbours.begin(),
+                                    not_fl_op_neighbours.end(),
+                                    neigh_not_fl_op_neighbours.begin(),
+                                    neigh_not_fl_op_neighbours.end(),
+                                    std::back_inserter(next_open));
+              }
+            }
+          }
+        }
+      }
+    }
   }
 
   void reset() noexcept {
-    next_open.clear();
-    next_flag.clear();
+    clear();
     board_ptr.reset();
   }
 
+  bool open_and_flag() {
+    if (next_flag.size() == 0 && next_open.size() == 0)
+      return false;
+
+    for (auto pos : next_flag) {
+      board_ptr->at(pos).set_flagged();
+    }
+    for (auto pos : next_open) {
+      board_ptr->open(pos);
+    }
+    clear();
+    return true;
+  }
+
 private:
+  void clear() noexcept {
+    next_open.clear();
+    next_flag.clear();
+  }
+
+  std::vector<pos_type> not_flagged_opened_neighbours(pos_type pos) const {
+    return board_ptr->tile_neighbours(pos, [this](const pos_type& p) {
+      return board_ptr->is_inside_bounds(p) && !board_ptr->at(p).is_flagged() &&
+             !board_ptr->at(p).is_open();
+    });
+  }
+
+  std::vector<pos_type> not_flagged_opened_neighbours(size_type idx) const {
+    return not_flagged_opened_neighbours(board_ptr->idx_to_pos(idx));
+  }
+
   std::vector<pos_type> next_open;
   std::vector<pos_type> next_flag;
-  std::weak_ptr<Mineboardbase> board_ptr;
-};
+  std::shared_ptr<Mineboardbase> board_ptr;
+}; // namespace rake
 
 } // namespace rake
 
