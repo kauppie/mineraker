32fd4e1b6eb76187501b863bd6c485931301fc88
eliaskauppi <elkku.kauppi@gmail.com>
2020-09-07 00:51:17 +0300

    Formatting changes

--- src/solver_iterator.hpp
+++ src/solver_iterator.hpp
@@ -10,7 +10,7 @@
 namespace rake {
 
 class SolverIterator {
-public:
+ public:
   using size_type = std::size_t;
   using diff_type = std::ptrdiff_t;
 
@@ -33,7 +33,6 @@ public:
     for (auto tile_it = board_ptr->begin(); tile_it != board_ptr->end();
          ++tile_it) {
       if (tile_it->is_open() && tile_it->is_number()) {
-
         auto all_neighbours = board_ptr->tile_neighbours(
             std::distance(board_ptr->begin(), tile_it));
 
@@ -51,7 +50,6 @@ public:
             auto& neigbour = board_ptr->at(*neigh_pos_it);
 
             if (neigbour.is_open() && neigbour.is_number()) {
-
               auto tile_flag_value =
                   tile_it->value() -
                   std::count_if(all_neighbours.begin(), all_neighbours.end(),
@@ -101,7 +99,6 @@ public:
     for (auto tile_it = board_ptr->begin(); tile_it != board_ptr->end();
          ++tile_it) {
       if (tile_it->is_open() && tile_it->is_number()) {
-
         auto not_fl_op_neighbours = not_flagged_opened_neighbours(
             std::distance(board_ptr->begin(), tile_it));
         auto all_neighbours = board_ptr->tile_neighbours(
@@ -125,10 +122,8 @@ public:
         } else {
           for (auto neigh_pos_it = all_neighbours.begin();
                neigh_pos_it != all_neighbours.end(); ++neigh_pos_it) {
-
             if (board_ptr->at(*neigh_pos_it).is_open() &&
                 board_ptr->at(*neigh_pos_it).is_number()) {
-
               auto neigh_not_fl_op_neighbours =
                   not_flagged_opened_neighbours(*neigh_pos_it);
               auto all_neigh_neighbours =
@@ -147,7 +142,6 @@ public:
                                 not_fl_op_neighbours.end(),
                                 neigh_not_fl_op_neighbours.begin(),
                                 neigh_not_fl_op_neighbours.end())) {
-
                 std::set_difference(not_fl_op_neighbours.begin(),
                                     not_fl_op_neighbours.end(),
                                     neigh_not_fl_op_neighbours.begin(),
@@ -167,8 +161,7 @@ public:
   }
 
   bool open_and_flag() {
-    if (next_flag.size() == 0 && next_open.size() == 0)
-      return false;
+    if (next_flag.size() == 0 && next_open.size() == 0) return false;
 
     for (auto pos : next_flag) {
       board_ptr->at(pos).set_flagged();
@@ -180,7 +173,7 @@ public:
     return true;
   }
 
-private:
+ private:
   void clear() noexcept {
     next_open.clear();
     next_flag.clear();
@@ -204,6 +197,6 @@ private:
   std::shared_ptr<Mineboardbase> board_ptr;
 };
 
-} // namespace rake
+}  // namespace rake
 
 #endif
